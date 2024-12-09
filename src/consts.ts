c3cfbcc0396cd26486face96a7a6774dcd5f9cd6
_Nik0__ <maciek.f137@gmail.com>
2024-12-09 19:50:15 +0100

    Add missing pages

---
+++ src/consts.ts
@@ -0,0 +1,109 @@
+import type { Site, Page, Links, Socials } from "@types"
+
+// Global
+export const SITE: Site = {
+  TITLE: "Sezam",
+  DESCRIPTION: "Randomness since 2018.",
+  AUTHOR: "Niko's Stuff",
+}
+
+// Work Page
+export const WORK: Page = {
+  TITLE: "Work",
+  DESCRIPTION: "Places I have worked.",
+}
+
+// Blog Page
+export const BLOG: Page = {
+  TITLE: "Blog",
+  DESCRIPTION: "Writing on topics I am passionate about.",
+}
+
+// Projects Page 
+export const EVENTS: Page = {
+  TITLE: "Wydarzenia",
+  DESCRIPTION: "Recent events I have worked on.",
+}
+
+// Team Page
+export const TEAM: Page = {
+  TITLE: "Team",
+  DESCRIPTION: "See our team.",
+}
+
+// Other Page
+export const OTHERPROJECTS: Page = {
+  TITLE: "Other Stuff",
+  DESCRIPTION: "Random unfinished events.",
+}
+
+// Search Page
+export const SEARCH: Page = {
+  TITLE: "Wyszukaj",
+  DESCRIPTION: "Search all posts and events by keyword.",
+}
+
+// NNB Workshop
+export const NNB_WORKSHOP: Page = {
+  TITLE: "NNB Workshop",
+  DESCRIPTION: "Explore modded content.",
+}
+
+// Links
+export const LINKS: Links = [
+  { 
+    TEXT: "Główna",
+    HREF: "/", 
+  },
+  {
+    TEXT: "Produkty",
+    HREF: "/produkty",
+  },
+  {
+    TEXT: "Lokalizacja",
+    HREF: "/lokalizacja",
+  },
+  {
+    TEXT: "O Nas",
+    HREF: "/onas",
+  },
+  {
+    TEXT: "Narzędzia",
+    HREF: "/narzedzia",
+  },
+]
+
+// Socials
+export const SOCIALS: Socials = [
+  // {
+  //   NAME: "Email",
+  //   ICON: "email",
+  //   TEXT: "team@nikostuff.com",
+  //   HREF: "mailto:team@nikostuff.com",
+  // },
+  // {
+  //   NAME: "Github",
+  //   ICON: "github",
+  //   TEXT: "",
+  //   HREF: ""
+  // },
+  // {
+  //   NAME: "LinkedIn",
+  //   ICON: "linkedin",
+  //   TEXT: "",
+  //   HREF: "",
+  // },
+  // {
+  //   NAME: "Twitter",
+  //   ICON: "twitter-x",
+  //   TEXT: "",
+  //   HREF: "",
+  // },
+  // {
+  //   NAME: "Discord",
+  //   ICON: "discord",
+  //   TEXT: "nikodaproot",
+  //   HREF: "https://dc.nikostuff.com",
+  // }
+]
+
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@ import defaultTheme from "tailwindcss/defaultTheme"
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ''),
+    files: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  },
   theme: {
     extend: {
       fontFamily: {
@@ -58,5 +62,10 @@ export default {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"),require('taos/plugin')],
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))'
+  ]
 }
